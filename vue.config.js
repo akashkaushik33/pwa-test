2200bc9a546cfd5adca72c2474a5cead9b9c4906
Akash Kaushik <akash.kaushik@indiqus.com>
2018-09-18 00:54:06 +0530

    pwa test

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,5 @@
 module.exports = {
   devServer: {
     disableHostCheck: true
-  },
-  pwa: {
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-        // swSrc is required in InjectManifest mode.
-        swSrc: '/service-worker.js',
-        // ...other Workbox options...
-    }
   }
 }
